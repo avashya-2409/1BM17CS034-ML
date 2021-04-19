496f2df6a85df3188a39131ea2f167e97ef18546
avashya-2409 <53857029+avashya-2409@users.noreply.github.com>
2021-04-19 10:00:31 +0530

    Add files via upload

---
+++ Week1/findS_alg.py
@@ -0,0 +1,13 @@
+Time Weather Temperature Company Humidity    Wind Goes
+0  Morning   Sunny        Warm     Yes     Mild  Strong  Yes
+1  Evening   Rainy        Cold      No     Mild  Normal   No
+2  Morning   Sunny    Moderate     Yes   Normal  Normal  Yes
+3  Evening   Sunny        Cold     Yes     High  Strong  Yes
+
+ n
+n The attributes are:  [['Morning' 'Sunny' 'Warm' 'Yes' 'Mild' 'Strong']
+                         ['Evening' 'Rainy' 'Cold' 'No' 'Mild' 'Normal']
+                  ['Morning' 'Sunny' 'Moderate' 'Yes' 'Normal' 'Normal']
+                       ['Evening' 'Sunny' 'Cold' 'Yes' 'High' 'Strong']]
+n The target is:  ['Yes' 'No' 'Yes' 'Yes']
+n The final hypothesis is: ['?' 'Sunny' '?' 'Yes' '?' '?']
